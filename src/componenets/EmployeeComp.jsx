faba5ff0cb4f1981626b566dc5105ae880360e1e
CM Soyza <cmsoyza@gmail.com>
2024-02-13 23:02:58 +0530

    Add form validations.

--- src/componenets/EmployeeComp.jsx
+++ src/componenets/EmployeeComp.jsx
@@ -8,6 +8,13 @@ const EmployeeComp = () => {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
 
+    // Initialize state variables that will hold validation errors
+    const [errors, setErros] = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
+
     const navigator = useNavigate();
 
     // function handleFirstName(e){
@@ -21,15 +28,47 @@ const EmployeeComp = () => {
     // }
     function saveEmployee(e) {
         e.preventDefault();
-        const employee = { firstName, lastName, email }
-        console.log(employee);
 
-        createEmployee(employee).then((response) => {
-            console.log(response.data);
-            navigator('/employees');
-        });
+        if (validateForm()) {
+            const employee = { firstName, lastName, email }
+            console.log(employee);
+            createEmployee(employee).then((response) => {
+                console.log(response.data);
+                navigator('/employees');
+            });
+        } 
+
+
     }
 
+    function validateForm() {
+        let valid = true;
+        const errrosCopy = { ...errors }
+
+
+        if (firstName.trim()) {
+            errrosCopy.firstName = '';
+        } else {
+            errrosCopy.firstName = 'First name is required!';
+            valid = false;
+        }
+
+        if (lastName.trim()) {
+            errrosCopy.lastName = '';
+        } else {
+            errrosCopy.lastName = 'Last name is required!';
+            valid = false;
+        }
+
+        if (email.trim()) {
+            errrosCopy.email = '';
+        } else {
+            errrosCopy.email = 'Email is required!';
+            valid = false;
+        }
+        setErros(errrosCopy);
+        return valid;
+    }
 
     return (
         <div className='container '><br /><br />
@@ -37,7 +76,7 @@ const EmployeeComp = () => {
                 <div className='card col-md-6 offset-md-3 mt-10 mb-10'>
                     <h1 className='text-center mb-10'>Add Employee</h1>
                     <div className='card-body'>
-                        <form action="">
+                        <form >
                             <div className='form-group mb-2'>
                                 <label htmlFor="firstName" className='form-label'>First Name : </label>
                                 <input
@@ -45,9 +84,10 @@ const EmployeeComp = () => {
                                     placeholder='Enter first name here'
                                     name='firstName'
                                     value={firstName}
-                                    className='form-control'
+                                    className={`form-control ${errors.firstName ? 'is-invalid':''}`}
                                     onChange={(e) => setFirstName(e.target.value)}
                                 />
+                                {errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
                             </div>
                             <div className='form-group mb-2'>
                                 <label htmlFor="lastName" className='form-label'>Last Name : </label>
@@ -56,9 +96,10 @@ const EmployeeComp = () => {
                                     placeholder='Enter last name here'
                                     name='lastName'
                                     value={lastName}
-                                    className='form-control'
+                                    className={`form-control ${errors.lastName ? 'is-invalid':''}`}
                                     onChange={(e) => setLastName(e.target.value)}
                                 />
+                                {errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
                             </div>
                             <div className='form-group mb-2'>
                                 <label htmlFor="email" className='form-label'>Email : </label>
@@ -67,9 +108,10 @@ const EmployeeComp = () => {
                                     placeholder='Enter email  here'
                                     name='email'
                                     value={email}
-                                    className='form-control'
+                                    className={`form-control ${errors.email ? 'is-invalid':''}`}
                                     onChange={(e) => setEmail(e.target.value)}
                                 />
+                                {errors.firstName && <div className='invalid-feedback'>{errors.email}</div>}
                             </div>
                             <button type='submit' className='btn btn-success' onClick={saveEmployee}>Submit</button>
                         </form>
